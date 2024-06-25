7c00b79f0acbe81a8b1a889dbdb46f130376899a
TME520 <tme520@gmail.com>
2024-06-25 22:32:41 +1000

    OpenCV: Template Matching with Multiple Objects (1)

---
+++ lab/template_matching001.py
@@ -0,0 +1,31 @@
+try:
+  import cv2 as cv
+except ImportError:
+  print('Cannot import cv2 computer vision PIP module\nTry: pip3 install opencv-python')
+
+try:
+  import numpy as np
+except ImportError:
+  print('Cannot import numpy module\nTry: pip3 install numpy') 
+
+try:
+  from matplotlib import pyplot as plt
+except ImportError:
+  print('Cannot import pyplot module\nTry: pip3 install matplotlib') 
+ 
+img_rgb = cv.imread('mario.png')
+assert img_rgb is not None, "file could not be read, check with os.path.exists()"
+img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+template = cv.imread('mario_coin.png', cv.IMREAD_GRAYSCALE)
+assert template is not None, "file could not be read, check with os.path.exists()"
+w, h = template.shape[::-1]
+ 
+res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+ cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+ 
+cv.imwrite('res.png',img_rgb)
+exit(0)
+
