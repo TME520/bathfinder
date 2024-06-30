a0909f8f52b0f9aa4df5bb3c7a7f467d60865c98
TME520 <tme520@gmail.com>
2024-06-30 16:25:40 +1000

    Added function to find & draw items on floorplan (2)

--- lab/template_matching005-generic.py
+++ lab/template_matching005-generic.py
@@ -53,12 +53,6 @@ if sink is None:
 sink_w, sink_h = sink.shape[::-1]
 
 spotanddraw(floorplan_rgb, floorplan_gray, sink, sink_w, sink_h, 0, 0, 255)
- 
-#res = cv.matchTemplate(floorplan_gray,sink,cv.TM_CCOEFF_NORMED)
-#threshold = 0.8
-#loc = np.where( res >= threshold)
-#for pt in zip(*loc[::-1]):
-#  cv.rectangle(floorplan_rgb, pt, (pt[0] + sink_w, pt[1] + sink_h), (0,0,255), 2)
 
 sinkrot90=os.path.dirname(args.sink) + '/' + os.path.splitext(os.path.basename(args.sink))[0] + '-rot90' + os.path.splitext(os.path.basename(args.sink))[1]
 sinkrot180=os.path.dirname(args.sink) + '/' + os.path.splitext(os.path.basename(args.sink))[0] + '-rot180' + os.path.splitext(os.path.basename(args.sink))[1]
@@ -67,10 +61,17 @@ if os.path.isfile(sinkrot90) is True:
   print(f"[INFO] Additional ROT90 file found: {sinkrot90}")
   sinkrot90img = cv.imread(cv.samples.findFile(sinkrot90), cv.IMREAD_GRAYSCALE)
   sinkrot90_w, sinkrot90_h = sinkrot90img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, sinkrot90img, sinkrot90_w, sinkrot90_h, 0, 0, 255)
 if os.path.isfile(sinkrot180) is True:
   print(f"[INFO] Additional ROT180 file found: {sinkrot180}")
+  sinkrot180img = cv.imread(cv.samples.findFile(sinkrot180), cv.IMREAD_GRAYSCALE)
+  sinkrot180_w, sinkrot180_h = sinkrot180img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, sinkrot180img, sinkrot180_w, sinkrot180_h, 0, 0, 255)
 if os.path.isfile(sinkrot270) is True:
   print(f"[INFO] Additional ROT270 file found: {sinkrot270}")
+  sinkrot270img = cv.imread(cv.samples.findFile(sinkrot270), cv.IMREAD_GRAYSCALE)
+  sinkrot270_w, sinkrot270_h = sinkrot270img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, sinkrot270img, sinkrot270_w, sinkrot270_h, 0, 0, 255)
 
 print(f"[INFO] Load shower from file {args.shower}")
 shower = cv.imread(cv.samples.findFile(args.shower), cv.IMREAD_GRAYSCALE)
@@ -78,22 +79,27 @@ if shower is None:
   print(f"[ERROR] Could not open or find the template shower image {args.shower}")
   exit(0)
 shower_w, shower_h = shower.shape[::-1]
- 
-res = cv.matchTemplate(floorplan_gray,shower,cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-  cv.rectangle(floorplan_rgb, pt, (pt[0] + shower_w, pt[1] + shower_h), (255,0,0), 2)
+
+spotanddraw(floorplan_rgb, floorplan_gray, shower, shower_w, shower_h, 255, 0, 0)
 
 showerrot90=os.path.dirname(args.shower) + '/' + os.path.splitext(os.path.basename(args.shower))[0] + '-rot90' + os.path.splitext(os.path.basename(args.shower))[1]
 showerrot180=os.path.dirname(args.shower) + '/' + os.path.splitext(os.path.basename(args.shower))[0] + '-rot180' + os.path.splitext(os.path.basename(args.shower))[1]
 showerrot270=os.path.dirname(args.shower) + '/' + os.path.splitext(os.path.basename(args.shower))[0] + '-rot270' + os.path.splitext(os.path.basename(args.shower))[1]
 if os.path.isfile(showerrot90) is True:
   print(f"[INFO] Additional ROT90 file found: {showerrot90}")
+  showerrot90img = cv.imread(cv.samples.findFile(showerrot90), cv.IMREAD_GRAYSCALE)
+  showerrot90_w, showerrot90_h = showerrot90img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, showerrot90img, showerrot90_w, showerrot90_h, 255, 0, 0)
 if os.path.isfile(showerrot180) is True:
   print(f"[INFO] Additional ROT180 file found: {showerrot180}")
+  showerrot180img = cv.imread(cv.samples.findFile(showerrot180), cv.IMREAD_GRAYSCALE)
+  showerrot180_w, showerrot180_h = showerrot180img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, showerrot180img, showerrot180_w, showerrot180_h, 255, 0, 0)
 if os.path.isfile(showerrot270) is True:
   print(f"[INFO] Additional ROT270 file found: {showerrot270}")
+  showerrot270img = cv.imread(cv.samples.findFile(showerrot270), cv.IMREAD_GRAYSCALE)
+  showerrot270_w, showerrot270_h = showerrot270img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, showerrot270img, showerrot270_w, showerrot270_h, 255, 0, 0)
 
 print(f"[INFO] Load bathtub from file {args.bathtub}")
 bathtub = cv.imread(cv.samples.findFile(args.bathtub), cv.IMREAD_GRAYSCALE)
@@ -102,21 +108,26 @@ if bathtub is None:
   exit(0)
 bathtub_w, bathtub_h = bathtub.shape[::-1]
  
-res = cv.matchTemplate(floorplan_gray,bathtub,cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-  cv.rectangle(floorplan_rgb, pt, (pt[0] + bathtub_w, pt[1] + bathtub_h), (0,255,0), 2)
+spotanddraw(floorplan_rgb, floorplan_gray, bathtub, bathtub_w, bathtub_h, 0, 255, 0)
 
 bathtubrot90=os.path.dirname(args.bathtub) + '/' + os.path.splitext(os.path.basename(args.bathtub))[0] + '-rot90' + os.path.splitext(os.path.basename(args.bathtub))[1]
 bathtubrot180=os.path.dirname(args.bathtub) + '/' + os.path.splitext(os.path.basename(args.bathtub))[0] + '-rot180' + os.path.splitext(os.path.basename(args.bathtub))[1]
 bathtubrot270=os.path.dirname(args.bathtub) + '/' + os.path.splitext(os.path.basename(args.bathtub))[0] + '-rot270' + os.path.splitext(os.path.basename(args.bathtub))[1]
 if os.path.isfile(bathtubrot90) is True:
   print(f"[INFO] Additional ROT90 file found: {bathtubrot90}")
+  bathtubrot90img = cv.imread(cv.samples.findFile(bathtubrot90), cv.IMREAD_GRAYSCALE)
+  bathtubrot90_w, bathtubrot90_h = bathtubrot90img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, bathtubrot90img, bathtubrot90_w, bathtubrot90_h, 0, 255, 0)
 if os.path.isfile(bathtubrot180) is True:
   print(f"[INFO] Additional ROT180 file found: {bathtubrot180}")
+  bathtubrot180img = cv.imread(cv.samples.findFile(bathtubrot180), cv.IMREAD_GRAYSCALE)
+  bathtubrot180_w, bathtubrot180_h = bathtubrot180img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, bathtubrot180img, bathtubrot180_w, bathtubrot180_h, 0, 255, 0)
 if os.path.isfile(bathtubrot270) is True:
   print(f"[INFO] Additional ROT270 file found: {bathtubrot270}")
+  bathtubrot270img = cv.imread(cv.samples.findFile(bathtubrot270), cv.IMREAD_GRAYSCALE)
+  bathtubrot270_w, bathtubrot270_h = bathtubrot270img.shape[::-1]
+  spotanddraw(floorplan_rgb, floorplan_gray, bathtubrot270img, bathtubrot270_w, bathtubrot270_h, 0, 255, 0)
  
 print("[INFO] Writing result file template_matching004-generic_output.png")
 cv.imwrite('template_matching004-generic_output.png',floorplan_rgb)
