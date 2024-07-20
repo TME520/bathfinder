59cfba8a9d12cc5647a4f90d259de7126c7446aa
TME520 <tme520@gmail.com>
2024-07-20 11:38:18 +1000

    Added: report.txt and report.html files
    - report.txt clearly states what was found and mentions which floorplans didn't yield any result
    - report.html is the same for browsers
    - Each run now creates its own folder in output

--- lab/thresholds.py
+++ lab/thresholds.py
@@ -5,9 +5,17 @@ thresholds_db = {
   'shower':0.7,
   'sink':0.7,
   'toilet':0.7,
-  'floorplan002-shower01.png':0.9,
-  'floorplan002-shower01-rot90.png':0.9,
-  'floorplan002-shower01-rot180.png':0.9,
-  'floorplan002-shower01-rot270.png':0.9
+  'floorplan002-shower01.png':0.95,
+  'floorplan002-shower01-rot90.png':0.95,
+  'floorplan002-shower01-rot180.png':0.95,
+  'floorplan002-shower01-rot270.png':0.95,
+  'floorplan002-sink01.png':0.9,
+  'floorplan002-sink01-rot90.png':0.9,
+  'floorplan002-sink01-rot180.png':0.9,
+  'floorplan002-sink01-rot270.png':0.9,
+  'floorplan002-sink02.png':0.9,
+  'floorplan002-sink02-rot90.png':0.9,
+  'floorplan002-sink02-rot180.png':0.9,
+  'floorplan002-sink02-rot270.png':0.9
 }
 
